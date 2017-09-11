016224a641590f6d5fceacd411291802b742dc3f
yq <120370694@qq.com>
2017-09-11 15:17:36 +0800

    change start class name

--- src/main/java/com/yq/wechat/WechatWpApplication.java
+++ src/main/java/com/yq/wechat/WechatWpApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class WechatWpApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(WechatWpApplication.class, args);
 	}
 
 }
